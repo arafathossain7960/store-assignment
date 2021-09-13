95aa4955c1fbb94d454b7cc9a3f0c3366d3dd119
Arafat Hossaine <arafathossain7960@gmail.comm>
2021-09-13 12:15:50 +0600

    second commit

--- js/app.js
+++ js/app.js
@@ -10,7 +10,8 @@ loadProducts();
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
+    console.log(product);
+    const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
@@ -20,64 +21,78 @@ const showProducts = (products) => {
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
+      <h4>Rating :${product.rating.count}</h4>
+      <h4>Average Rating :${product.rating.rate}</h4>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
 };
+
+// addToCart onclick function------ 
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
   updatePrice("price", price);
-
   updateTaxAndCharge();
+  updateTotal();
   document.getElementById("total-Products").innerText = count;
 };
-
+// get value from document.id  function
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = element;
   return converted;
 };
 
 // main price update function
-const updatePrice = (id, value) => {
+const updatePrice = (id, price) => {
+
   const convertedOldPrice = getInputValue(id);
-  const convertPrice = parseFloat(value);
-  const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  const convertPrice = price;
+  const total = parseFloat(convertedOldPrice) + parseFloat(convertPrice);
+  document.getElementById(id).innerText = twoDecimalAndParsFloat(total);
 };
 
-// set innerText function
+// set deliver charge and total-tax function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText =value;
 };
 
 // update delivery charge and total Tax
 const updateTaxAndCharge = () => {
-  const priceConverted = getInputValue("price");
+  const priceConverted =  getInputValue("price");
+  console.log(priceConverted)
   if (priceConverted > 200) {
     setInnerText("delivery-charge", 30);
-    setInnerText("total-tax", priceConverted * 0.2);
+    setInnerText("total-tax", twoDecimalAndParsFloat(priceConverted * 0.2));
   }
   if (priceConverted > 400) {
     setInnerText("delivery-charge", 50);
-    setInnerText("total-tax", priceConverted * 0.3);
+    setInnerText("total-tax",twoDecimalAndParsFloat(priceConverted * 0.3));
   }
   if (priceConverted > 500) {
     setInnerText("delivery-charge", 60);
-    setInnerText("total-tax", priceConverted * 0.4);
+    setInnerText("total-tax",twoDecimalAndParsFloat(priceConverted * 0.4) );
   }
 };
 
 //grandTotal update function
 const updateTotal = () => {
   const grandTotal =
-    getInputValue("price") + getInputValue("delivery-charge") +
-    getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+  parseFloat(getInputValue("price"))+
+  parseFloat(getInputValue("delivery-charge"))+
+ parseFloat( getInputValue("total-tax"));
+   
+  document.getElementById("total").innerText =twoDecimalAndParsFloat(grandTotal);
+  console.log(grandTotal)
 };
 
 
-// i have done cloning step in js 
+// toFixed function 
+const twoDecimalAndParsFloat=money=>{
+  const convertMoney  = parseFloat(money).toFixed(2);
+
+  return convertMoney ;
+}
